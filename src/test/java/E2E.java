e123e6258e27c9032f7fc34572b961b4774cf69a
srikanth <srikanth8494@gmail.com>
2022-08-03 13:24:28 +0530

    Initial push2

--- src/test/java/E2E.java
+++ src/test/java/E2E.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 public class E2E
 {
     public static void main(String[] args) throws InterruptedException, AWTException {
-//test
+//test1
         System.setProperty("webdriver.chrome.driver","Drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
